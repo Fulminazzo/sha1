31121948d69f4d16222a6eff3a3f721fe3e749d2
Fulminazzo <fulminazzo21@gmail.com>
2024-03-05 19:57:07 +0100

    Initial commit

---
+++ sha1.c
@@ -0,0 +1,7 @@
+#include "sha1.h"
+
+#include <stdio.h>
+
+void hello(void) {
+    printf("Hello, World!\n");
+}
---
+++ sha1.h
@@ -0,0 +1,6 @@
+#ifndef SHA1_SHA1_H
+#define SHA1_SHA1_H
+
+void hello(void);
+
+#endif //SHA1_SHA1_H
