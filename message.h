37b8dd927692d71a3fb4fef9057e82b56c915f90
Fulminazzo <fulminazzo21@gmail.com>
2024-03-06 00:03:57 +0100

    Fixed preprocess_message return type Added deprocess_message function

--- message.h
+++ message.h
@@ -10,11 +10,15 @@
 #define SEPARATOR 128
 #define PADDING 0
 
+#define INVALID_LENGTH_PROVIDED 100
+
 #include "utils.h"
 
 /**
  * @return the final size of the message
  */
-unsigned long int preprocess_message(uchar *dst, const char *src);
+ulong preprocess_message(uchar *dst, const char *src);
+
+void deprocess_message(char *dst, const uchar *src, ulong size);
 
 #endif //SHA1_MESSAGE_H
