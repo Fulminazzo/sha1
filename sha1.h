4b76a4880797b91f1b3ba323336727dd8ed9a290
Fulminazzo <fulminazzo21@gmail.com>
2024-03-06 00:35:06 +0100

    Created Message union

--- sha1.h
+++ sha1.h
@@ -1,6 +1,14 @@
 #ifndef SHA1_SHA1_H
 #define SHA1_SHA1_H
 
-void hello(void);
+#define MESSAGE_SIZE 65536
+
+#include "functions.h"
+#include "message.h"
+
+typedef union Message_s {
+    uchar bytes[MESSAGE_SIZE];
+    WORD words[MESSAGE_SIZE / sizeof(WORD)];
+} Message;
 
 #endif //SHA1_SHA1_H
