895d743a27e2d527541441284fdb4fa5338ecdb7
Fulminazzo <fulminazzo21@gmail.com>
2024-03-06 00:26:59 +0100

    Added s function

--- functions.c
+++ functions.c
@@ -1,1 +1,5 @@
 #include "functions.h"
+
+WORD s(WORD x, int n) {
+    return (x << n) | (x >> (32 - n));
+}
--- functions.h
+++ functions.h
@@ -1,4 +1,8 @@
 #ifndef SHA1_FUNCTIONS_H
 #define SHA1_FUNCTIONS_H
 
+#define WORD int
+
+WORD s(WORD x, int n);
+
 #endif //SHA1_FUNCTIONS_H
