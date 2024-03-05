71d4d3ef171ca8f330e6f712e5c1606f83f2bc9f
Fulminazzo <fulminazzo21@gmail.com>
2024-03-05 21:39:43 +0100

    Added preprocess_message function

--- message.c
+++ message.c
@@ -1,1 +1,32 @@
 #include "message.h"
+#include <string.h>
+
+int power(const int n, const int i) {
+    if (i == 0) return 1;
+    else if (i <= 1) return n;
+    else return n * power(n, i - 1);
+}
+
+void preprocess_message(char *dst, const char *src) {
+    const size_t length = strlen(src);
+    size_t i, bits;
+    const int mask = power(2, BITS_PER_BYTE) - 1;
+    int shift;
+
+    for (i = 0; i != length; i++) dst[i] = src[i];
+
+    // Set separator bit
+    dst[i++] = (char) SEPARATOR;
+
+    while ((i + BYTES_SIZE_LENGTH) % BYTES_LENGTH != 0)
+        dst[i++] &= PADDING;
+
+    // Convert length to bits
+    bits = length * BITS_PER_BYTE;
+
+    for (int j = 0; j < BYTES_SIZE_LENGTH; j++) {
+        shift = BYTES_SIZE_LENGTH - j - 1;
+        shift *= BITS_PER_BYTE;
+        dst[i + j] = (char) (bits & ((size_t) mask << shift));
+    }
+}
--- message.h
+++ message.h
@@ -1,4 +1,13 @@
 #ifndef SHA1_MESSAGE_H
 #define SHA1_MESSAGE_H
 
+#define BITS_PER_BYTE 8
+
+#define BYTES_LENGTH 64
+#define BYTES_SIZE_LENGTH 8
+#define SEPARATOR 128
+#define PADDING 0
+
+void preprocess_message(char *dst, const char *src);
+
 #endif //SHA1_MESSAGE_H
