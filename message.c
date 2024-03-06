cee7bc3d985902175bc85ae3e3c35a0334c403e2
Fulminazzo <fulminazzo21@gmail.com>
2024-03-06 03:28:18 +0100

    Corrected preprocess_message method padding

--- message.c
+++ message.c
@@ -3,27 +3,31 @@
 #include <stdlib.h>
 
 ulong preprocess_message(uchar *dst, const char *src) {
-    const ulong length = strlen(src);
+    const ulong length = strlen(src), size = sizeof(unsigned int);
     ulong i, bits;
-    int c, *tmp;
+    unsigned int c, *tmp;
 
-    tmp = (int *) dst;
-    for (i = 0; i < length; i += sizeof(int)) {
+    tmp = (unsigned int *) dst;
+
+    for (i = 0; i < length; i += size) {
         c = chars_to_int(src + i);
-        tmp[i / sizeof(int)] = c;
+        tmp[i / size] = c;
     }
     if (i > length + 1) i = length + 1;
 
     // Set separator bit
     dst[0] = (char) SEPARATOR;
 
+    // Set padding
     while ((i + BYTES_SIZE_LENGTH) % BYTES_LENGTH != 0)
-        dst[i++] &= PADDING;
+        dst[i++] = PADDING;
 
     // Convert length to bits
     bits = length * BITS_PER_BYTE;
 
     write_to_string(dst, bits, i);
+    tmp[i / size] = 0;
+    tmp[i / size + 1] = bits;
 
     return i + BYTES_SIZE_LENGTH;
 }
